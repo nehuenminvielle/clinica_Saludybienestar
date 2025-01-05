49ffc570f691e119e282b97c891ced30f7f193eb
Nehuen Minvielle <nehuenalexism@gmail.com>
2025-01-05 14:20:44 -0300

    settings.py

--- ProyectoSalud/ProyectoSalud/settings.py
+++ ProyectoSalud/ProyectoSalud/settings.py
@@ -20,7 +20,7 @@ import os
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviroment.get('SECRET_KEY', default='geetaergeteeahhth')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='geetaergeteeahhth')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
